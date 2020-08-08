142349e83cd9b65effe28a7b9d9db8236c158f92
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-08 13:36:55 +0900

    feat: TODOが作成されたとき表示

--- front/src/components/TodoList.vue
+++ front/src/components/TodoList.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="field is-grouped mt-6">
-    <p class="control is-expanded">
-      <input v-model="todo.title" class="input" type="text" placeholder="Add Todo">
-    </p>
-    <p class="control">
-      <a class="button is-link" @click="createTodo">
-        Add
-      </a>
-    </p>
-    <ul v-for="todo in todos" :key="todo.id">
-      <li>{{ todo.title }}</li>
-    </ul>
-
+  <div>
+    <div class="field is-grouped mt-6">
+      <p class="control is-expanded">
+        <input v-model="todo.title" class="input" type="text" placeholder="Add Todo">
+      </p>
+      <p class="control">
+        <a class="button is-link" @click="createTodo">
+          Add
+        </a>
+      </p>
+    </div>
+    <div class="content">
+      <ul v-for="todo in todos" :key="todo.id">
+        <li>{{ todo.title }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -41,7 +44,15 @@ export default {
       axios
         .post('http://localhost:5000/v1/todos', this.todo)
         .then(res => {
-          console.log('Created!');
+          this.updateTodos();
+          this.todo.title = '';
+        })
+    },
+    updateTodos: function() {
+      axios
+        .get('http://localhost:5000/v1/todos')
+        .then(res => {
+          this.todos = res.data;
         })
     }
   }
