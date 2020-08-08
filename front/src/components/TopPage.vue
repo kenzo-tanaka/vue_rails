0d283ba78de012a622b40b6e80e180b8b531ae3e
kenzo <kenzou.kenzou104809@gmail.com>
2020-08-08 12:18:30 +0900

    feat: formを作成

--- front/src/components/TopPage.vue
+++ front/src/components/TopPage.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
+  <div class="container">
+    <TodoList />
   </div>
 </template>
 
 <script>
+import TodoList from '@/components/TodoList'
+
 export default {
   name: 'TopPage',
+  components: {
+    TodoList,
+  }
 }
 </script>
 
